141f35a2777233d39b84684d05b7fbd28aec94d1
Robin Deeboonchai <comtalyst@outlook.com>
2020-07-03 15:01:07 +0700

    Draft layout

--- src/App.js
+++ src/App.js
@@ -2,23 +2,30 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const draftStyle = {
+  textAlign: 'center'
+};
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={draftStyle}>
+      <h1>Navigation bar</h1>
+      <hr/>
+      <h2>TL;DR</h2>
+      <hr/>
+      <h2>Profile</h2>
+      <hr/>
+      <h2>Education/Courseworks</h2>
+      <hr/>
+      <h2>Tech Proficiency</h2>
+      <hr/>
+      <h2>Projects</h2>
+      <hr/>
+      <h2>Relevant Experiences</h2>
+      <hr/>
+      <h2>Honors</h2>
+      <hr/>
+      <h2>Contact</h2>
     </div>
   );
 }
