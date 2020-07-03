1605259175f289d32f43d9dbc0c76d572e224b46
Robin Deeboonchai <comtalyst@outlook.com>
2020-07-03 18:50:25 +0700

    Fix Navbar

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,31 +1,36 @@
 import React from 'react';
-import { Button, Box, Text, Flex } from "@chakra-ui/core";
+import { Button, Text, Flex } from "@chakra-ui/core";
 
 function NavigationBar() {
   return (
-    <Flex bg='reactDark' w='100%' color='white' py='0' justifyContent='space-between' px='1.5rem' h='4rem' alignItems='center'>
-      <Flex>
-        <Text fontSize='xl'>
-          Rawin (Robin) Deeboonchai
-        </Text>
-      </Flex>
-      <Flex h='100%'>
-        <Button
-          variant='ghost' color='white'
-          _hover={{bg: '#FFFFFF10'}} rounded='1px' h='100%'
-          _active={{ transform: 'scale(0.9)'}}
-          _focus={{ boxShadow: '0 0 0 0'}}
-        >
-          Profile
-        </Button>
-        <Button
-          variant='ghost' color='white'
-          _hover={{bg: '#FFFFFF10'}} rounded='1px' h='100%'
-          _active={{ transform: 'scale(0.9)'}}
-          _focus={{ boxShadow: '0 0 0 0'}}
-        >
-          Education
-        </Button>
+    <Flex>
+      <Flex h='4rem'></Flex>
+      <Flex bg='reactDark' w='100%' color='white' py='0' justifyContent='space-between' px='1.5rem' h='4rem' 
+        alignItems='center' position='fixed'
+      >
+        <Flex>
+          <Text fontSize='xl'>
+            Rawin (Robin) Deeboonchai
+          </Text>
+        </Flex>
+        <Flex h='100%'>
+          <Button
+            variant='ghost' color='white'
+            _hover={{bg: '#FFFFFF10'}} rounded='1px' h='100%'
+            _active={{ transform: 'scale(0.9)'}}
+            _focus={{ boxShadow: '0 0 0 0'}}
+          >
+            Profile
+          </Button>
+          <Button
+            variant='ghost' color='white'
+            _hover={{bg: '#FFFFFF10'}} rounded='1px' h='100%'
+            _active={{ transform: 'scale(0.9)'}}
+            _focus={{ boxShadow: '0 0 0 0'}}
+          >
+            Education
+          </Button>
+        </Flex>
       </Flex>
     </Flex>
   )
