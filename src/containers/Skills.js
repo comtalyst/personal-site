3f7c54304b8ad254460c03ec781b79734e04f3d4
Robin Deeboonchai <comtalyst@outlook.com>
2020-07-05 17:17:13 +0700

    Systemize skills inputting with the same method

--- src/containers/Skills.js
+++ src/containers/Skills.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Button, Text, Box, Flex, Image, Grid, Collapse } from "@chakra-ui/core";
 
 function Skills() {
+  const languages = [
+    'Javascript', 'Python'  
+  ]
 
   return (
     <Flex w='100%' pb='3rem' pt='1rem' direction='column'>
@@ -13,11 +16,15 @@ function Skills() {
           Languages
         </Text>
         <Flex pt='1rem' justifyContent='center'>
-          <Box borderWidth='2px' px='5px' py='1px' borderRadius='5px'>
-            <Text fontSize='xl' lineHeight='taller' letterSpacing='wider' fontWeight='light'>
-              JavaScript
-            </Text>
-          </Box>
+          {
+            languages.map(language =>
+              (<Box borderWidth='2px' px='5px' py='1px' borderRadius='5px' mx='2px' borderColor='blue.500'>
+                <Text fontSize='xl' lineHeight='taller' letterSpacing='wider' fontWeight='light'>
+                  {language}
+                </Text>
+              </Box>)
+            )
+          }
         </Flex>
       </Flex>
     </Flex>
