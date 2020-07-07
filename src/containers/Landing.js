5cb32c7b9810a96d9fa3669f3498091f5a733b5c
Robin Deeboonchai <comtalyst@outlook.com>
2020-07-07 16:14:59 +0700

    Fix landing text responsivity

--- src/containers/Landing.js
+++ src/containers/Landing.js
@@ -10,19 +10,11 @@ function Landing() {
   return (
     <Flex w='100%' h={window.innerHeight - 64} bgImage={"url(" + scene + ")"} bgRepeat="no-repeat" bgSize='cover'>
     <Flex w='100%' pb='3rem' pt='1rem' h='100%' direction='column' alignItems='center' justifyContent='center' bg='bgCover'>
-      <Flex mx='20px' direction='column' justifyContent='center' h='100%'>
+      <Flex mx='10px' direction='column' justifyContent='center' h='100%'>
         <Title text='Welcome!'/>
-        <Flex justifyContent='center' alignItems='center'>
-          <Text fontSize='xl' lineHeight='taller' letterSpacing='widest' fontWeight='light' textAlign='center'>
-            This is the portfolio of&nbsp;
-          </Text>
-          <Text fontSize='xl' lineHeight='taller' letterSpacing='widest' fontWeight='light' textAlign='center' color='blue.300'>
-            Robin D
-          </Text>
-          <Text fontSize='xl' lineHeight='taller' letterSpacing='widest' fontWeight='light' textAlign='center'>
-            .
-          </Text>
-        </Flex>
+        <Text fontSize='xl' lineHeight='taller' letterSpacing='widest' fontWeight='light' textAlign='center' wrap='wrap'>
+          This is the portfolio of <span style={{color: '#63B3ED'}}>Robin D.</span>
+        </Text>
         
         <Flex justifyContent='center' alignItems='center' pt='30px'>
           <Link isExternal href='https://www.facebook.com/comtalyst'><Box as={FaFacebook} size='30px' mx='10px'/></Link>
@@ -31,11 +23,9 @@ function Landing() {
           <Link isExternal href='https://www.linkedin.com/in/rawin-deeboonchai-41619a197/'><Box as={FaLinkedin} size='30px' mx='10px'/></Link>
         </Flex>
       </Flex>
-      <Flex mx='20px' direction='column' justifyContent='flex-end'>
-        <Text lineHeight='taller' letterSpacing='widest' fontWeight='light' textAlign='center' color='gray.400'>
-          Scroll down or use the navigation bar above to begin
-        </Text>
-      </Flex>
+      <Text mx='10px' lineHeight='taller' letterSpacing='widest' fontWeight='light' textAlign='center' color='gray.400' wrap='wrap'>
+        Scroll down or use the navigation bar above to begin
+      </Text>
     </Flex></Flex>
   )
 }
