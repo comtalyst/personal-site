ed70dd2c8b16068014b7b5f925c6f478cbc45737
Robin Deeboonchai <comtalyst@outlook.com>
2020-08-05 22:13:37 +0700

    Fix again

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
+import { HashRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 
 import customTheme from './CustomTheme.js'
 import { ThemeProvider } from '@chakra-ui/core'
@@ -10,14 +10,13 @@ import Home from './pages/Home.js';
 import GFC from './pages/GFC.js';
 
 function App() {
-  const pref = process.env.PUBLIC_URL
   return (
     <ThemeProvider theme={customTheme}>
       <Router>
         <Switch>
-          <Route exact path={pref + "/home"} component={Home}/>
-          <Route exact path={pref + "/pages/GFC"} component={GFC}/>
-          <Route exact path={pref + ["/", "//"]}><Redirect to={pref + "/home"}/></Route>
+          <Route exact path="/home" component={Home}/>
+          <Route exact path="/pages/GFC" component={GFC}/>
+          <Route exact path={["/", "//"]}><Redirect to="/home"/></Route>
           <Route component={() => (<span>Page Not Found</span>)}/>
         </Switch>
       </Router>
